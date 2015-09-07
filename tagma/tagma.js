cf18044c52de2f05651b0f7282de89a5b4268a0f
Richard Smith-Unna <richardsmith404@gmail.com>
2015-09-07 15:28:08 +0100

    Basic task functionality and interface

--- tagma/tagma.js
+++ tagma/tagma.js
@@ -1,10 +1,47 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // This code only runs on the client
+
   Template.taskList.helpers({
-    tasks: [
-      { title: "This is task 1", project: "Proj A", content: "" },
-      { title: "This is task 2", project: "Proj B", content: "" },
-      { title: "This is task 3", project: "Proj B", content: "" }
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
   });
+
+  Template.taskPopout.events({
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
+
+  Template.taskPopout.rendered = function () {
+    $('.collapsible').collapsible();
+  }
+
+  Template.body.events({
+    "submit #add-form": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var title = event.target.title.value;
+      var project = event.target.project.value;
+      var content = event.target.content.value;
+
+
+      // Insert a task into the collection
+      Tasks.insert({
+        title: title,
+        project: project,
+        content: content,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.reset();
+      setTimeout(function() { $('#add-panel').hide(); }, 300);
+    }
+  });
+
 }
