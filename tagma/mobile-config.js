6b672accb7a34ba824e8a31a4f555a5dd2cfab3c
Richard Smith-Unna <richardsmith404@gmail.com>
2015-09-13 16:28:35 +0100

    Add mobile config

---
+++ tagma/mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  name: 'Tagma',
+  description: 'The task manager you always wanted',
+  version: '0.0.1'
+});
+
+App.icons({
+  'android_ldpi': 'resources/icons/icon-ldpi.png',
+  'android_mdpi': 'resources/icons/icon-mdpi.png',
+  'android_hdpi': 'resources/icons/icon-hdpi.png',
+  'android_xhdpi': 'resources/icons/icon-xhdpi.png'
+});
